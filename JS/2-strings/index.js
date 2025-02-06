dd9b97c9fa80d33a874792aab14f8f39ad231a5d
mosesgitonga <infosec947@gmail.com>
2025-02-07 01:26:22 +0300

    update: camelize

--- JS/2-strings/index.js
+++ JS/2-strings/index.js
@@ -91,21 +91,80 @@ const capitalize = (some_string) => {
     let new_str = some_string.replace(some_string[0], first_letter)
     return new_str
 }
+
+const capitalize_Words = (some_string) => {
+    const words = some_string.split(' ')
+    let myArray = [];
+    for (let i = 0; i < words.length; i++) {
+        let word = capitalize(words[i])
+        myArray.push(word)
+    }
+    let capitalizedWords = myArray.join(' ')
+    return capitalizedWords 
+}
+
+
+const swapcase = (some_string) => {
+    let new_str
+    let myArray = []
+    for (let i = 0; i < some_string.length; i++) {
+        if (some_string[i] === some_string[i].toLowerCase()) {
+            small_char = some_string[i].toUpperCase()
+            myArray.push(small_char)
+        } 
+        cap_char = some_string[i].toLowerCase()
+        myArray.push(cap_char)
+    }
+    let res = myArray.join('')
+    return  res
+}
+
+const camelize = (some_string) => {
+    let sliced = some_string.split(' ')
+    console.log(sliced)
+    const concated = sliced.join('')
+    return concated
+}
+
+const uncamelize = (some_string, replacement) => {
+    let res;
+    let strings = some_string.slice(some_string[i])
+
+    for (let i = 0; i < strings.length; i++) {
+        if (some_string[i] === strings[i].toUpperCase() && strings[i] !== 0) {
+            console.log(some_string[i])
+            console.log(strings)
+            // let concated = strings.join('-')
+            // res = concated.toLowerCase()
+        }
+    }
+    return res 
+}
+
+
 // Test
-console.log(is_string("W3resource")); // true
-console.log(is_string(2)); // false
+// console.log(is_string("W3resource")); // true
+// console.log(is_string(2)); // false
 
-console.log(is_Blank('')); // true
-console.log(is_Blank('abc')); // false
+// console.log(is_Blank('')); // true
+// console.log(is_Blank('abc')); // false
 
-console.log(string_to_array("Robin Singh"));
+// console.log(string_to_array("Robin Singh"));
 
-console.log(truncate_string("Robin Singh", 4)); // "Robi"
+// console.log(truncate_string("Robin Singh", 4)); // "Robi"
 
-console.log(abbrev_name("Robin Singh")); // "Robin S."
+// console.log(abbrev_name("Robin Singh")); // "Robin S."
 
-console.log(protect_email("robin_singh@example.com"));
+// console.log(protect_email("robin_singh@example.com"));
 
-console.log(string_parameterize("Robin Singh from USA."));
+// console.log(string_parameterize("Robin Singh from USA."));
 
-console.log(capitalize('js string exercises'));
+// console.log(capitalize('js string exercises'));
+
+//console.log(capitalize_Words('js string exercises')); // "Js String Exercises"
+
+// console.log(swapcase('AaBbc')); // "aAbBC"
+
+// console.log(camelize("JavaScript Exercises")); // "JavaScriptExercises"
+
+console.log(uncamelize('helloWorld','-')); // "hello-world"
